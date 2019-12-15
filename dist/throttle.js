629f8013482332ec755d1cc036c48e9e6f61e1a8
ursarik <rosenwtf@gmail.com>
2019-12-15 16:59:53 +0200

    removed setTimeout

--- dist/throttle.js
+++ dist/throttle.js
@@ -1,15 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const throttle = (msec, fn) => {
-    let wait = false;
+    let lastCalled = null;
     return (...args) => {
-        if (!wait) {
-            wait = true;
-            setTimeout(() => {
-                wait = false;
-            }, msec);
-            return fn(...args);
-        }
+        const now = Date.now();
+        if (lastCalled && now - lastCalled < msec)
+            return;
+        lastCalled = now;
+        return fn(...args);
     };
 };
 exports.throttle = throttle;
